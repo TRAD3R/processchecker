d8116f5098c821a72ac8683ee1f79b26f967b942
trad3r <c1mba@yandex.ru>
2025-01-20 08:17:36 +0300

    fix: back to v1.2.1

--- processchecker.go
+++ processchecker.go
@@ -1,20 +1,42 @@
 package processchecker
 
 import (
+	"errors"
 	"fmt"
+	"log/slog"
 	"os"
 	"strconv"
 )
 
+type Logger struct {
+	*slog.Logger
+}
+
 func Run(pidFile string) error {
-	filepath, err := checkProcess(pidFile)
-	if err != nil {
-		return err
+	// Проверка существования PID файла
+	if _, err := os.Stat(pidFile); err == nil {
+		data, err := os.ReadFile(pidFile)
+		if err != nil {
+			return fmt.Errorf("failed to read PID file: %w", err)
+		}
+		pid, err := strconv.Atoi(string(data))
+		if err != nil {
+			if errR := os.Remove(pidFile); errR != nil {
+				err = errors.Join(err, fmt.Errorf("failed to remove PID file %s: %w", pidFile, errR))
+			}
+
+			return fmt.Errorf("iInvalid PID in PID file: %w", err)
+		}
+
+		// Проверка, активен ли процесс с этим PID
+		if err := checkProcess(pid); err != nil {
+			return err
+		}
 	}
 
-	// Запись текущего PID в PID файл
+	// Шаг 2: Запись текущего PID в PID файл
 	pid := os.Getpid()
-	err = os.WriteFile(filepath, []byte(strconv.Itoa(pid)), 0644)
+	err := os.WriteFile(pidFile, []byte(strconv.Itoa(pid)), 0644)
 	if err != nil {
 		return fmt.Errorf("Failed to write %d in PID file: %w\n", pid, err)
 	}
